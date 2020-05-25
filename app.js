bfd4a1670c0bbce26776e37271cf89f5adb25d29
Alan Margolies <amargolies88@gmail.com>
2020-05-25 19:23:12 -0400

    front end getting there

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const userRouter = require('./routes/User');
 
 var app = express();
@@ -22,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
