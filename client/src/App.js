8f413322984e17c7e7d8b29dc10099e9c5b49796
Alan Margolies <amargolies88@gmail.com>
2020-05-26 14:42:41 -0400

    completed mern auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { useContext } from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Todos from './Components/Todos';
+import Admin from './Components/Admin';
+import PrivateRoute from './hocs/PrivateRoute';
+import UnPrivateRoute from './hocs/UnPrivateRoute';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +14,10 @@ function App() {
     <Router>
       <Navbar />
       <Route exact path="/" component={Home} />
+      <UnPrivateRoute path="/login" component={Login} />
+      <UnPrivateRoute path="/register" component={Register} />
+      <PrivateRoute path="/todos" roles={["user", "admin"]} component={Todos} />
+      <PrivateRoute path="/admin" roles={["admin"]} component={Admin} />
     </Router>
   );
 }
