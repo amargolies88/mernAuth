01336ce6980d9514273849a794c3080889ac8a60
Alan Margolies <amargolies88@gmail.com>
2020-05-29 02:17:43 -0400

    good one

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '/client/build')));
+} else {
+  app.use(express.static(path.join(__dirname, 'public')));
 }
 
 
@@ -28,9 +29,9 @@ app.use('/user', userRouter);
 
 // Define all API routes before this runs
 // Send every request to the React app
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, 'public'));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, './client/index.html'));
+// });
 
 mongoose.connect(
   process.env.MONGODB_URI ||
