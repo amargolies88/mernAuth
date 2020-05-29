26dea771c32b3b86910f2bf5dfcce99f060f8493
Alan Margolies <amargolies88@gmail.com>
2020-05-29 01:57:57 -0400

    sh

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/user', userRouter);
 // Define all API routes before this runs
 // Send every request to the React app
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/index.html"));
+  res.sendFile(path.join(__dirname, 'public'));
 });
 
 mongoose.connect(
